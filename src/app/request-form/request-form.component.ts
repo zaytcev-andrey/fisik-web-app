e0c2df65b79a132ca6ea45c4cdb33adcab81c38d
andrey-zaytcev <andrey-zaytcev@narod.ru>
2020-12-21 23:36:27 +0300

    first improving front-end

--- src/app/request-form/request-form.component.ts
+++ src/app/request-form/request-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { PanelModule } from 'primeng/panel';
+import { InputTextModule } from 'primeng/inputtext';
 import { ButtonModule } from 'primeng/button';
 import { CalendarModule } from 'primeng/calendar';
 import { DropdownModule } from 'primeng/dropdown';
